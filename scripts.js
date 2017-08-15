65d047fc05aa89990dd7775b14cc2d14222c1fe0
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-15 15:35:18 -0500

    Fixed from ID to targeting Class

--- scripts.js
+++ scripts.js
@@ -1,25 +1,22 @@
 window.addEventListener('load', function() {
 	// change the text of that link from 'like' to 'Unlike'
-	document.getElementById("like").addEventListener("click", function(e) {
-    
-    var like = document.getElementById("like");
-    var likeVisible = like.className.indexOf("hidden") == -1;
-    
-    if (likeVisible) {
-        this.innerHTML = "Unlike";
-        like.className = "hidden";
-    } else {
-        this.innerHTML = "Like";
-        like.className = "";
-    }
-
-}, false);
+var like = document.getElementsByClassName("action action--like")[0];
 	
+	like.addEventListener("click", function() {
+		
+		if(like.innerHTML === "Like"){
+			like.innerHTML = "Unlike";
+		}
+		else {
+			like.innerHTML = "Like";
+		}
+		
+	});
+});	
 // update the number of likes that the post/comment has
 
-
 // alter the behavior of the link so it behaves as an Unlike link next time it's clicked
-});
+
 		
 		
 	
