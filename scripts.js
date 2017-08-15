045752963891c84d4aa12262388b00adeff70462
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-15 14:46:00 -0500

    Finally got the like/unlike button

--- scripts.js
+++ scripts.js
@@ -1,31 +1,25 @@
 window.addEventListener('load', function() {
 	// change the text of that link from 'like' to 'Unlike'
-
-
-	addEventListener('click', function(){
-		var show = document.getElementsByClassName('action--like');
-		var hide = document.getElementsByClassName('action--unlike');
-		
-for (var i = 0; i < show.length; i++) {
-      if (show[i].style.display === "none") {
-        show[i].style.display = "inline-block";
-      }
-      else {
-        show[i].style.display = "none";
-      }
+	document.getElementById("like").addEventListener("click", function(e) {
+    
+    var like = document.getElementById("like");
+    var likeVisible = like.className.indexOf("hidden") == -1;
+    
+    if (likeVisible) {
+        this.innerHTML = "Unlike";
+        like.className = "hidden";
+    } else {
+        this.innerHTML = "Like";
+        like.className = "";
     }
 
-		// show.onclick = function() {
-  //       	show.setAttribute('class', 'hide');
-  //       	hide.setAttribute('class', 'show');
-  //   	};
-  //   	hide.onclick = function() {
-  //       	hide.setAttribute('class', 'hide');
-  //       	show.setAttribute('class', 'show');
-  //   	};
-	});
+}, false);
+	
 // update the number of likes that the post/comment has
+
+
 // alter the behavior of the link so it behaves as an Unlike link next time it's clicked
 });
 		
 		
+	
